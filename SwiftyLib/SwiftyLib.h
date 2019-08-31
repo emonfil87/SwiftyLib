54ffe685006eb1d04948496e7f9f94d6cf3efed6
emonfil <emonfil@yosoyblueicon.com>
2019-08-31 13:22:13 -0500

    initial project setup

---
+++ SwiftyLib/SwiftyLib.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyLib.h
+//  SwiftyLib
+//
+//  Created by Erick Monfil on 8/31/19.
+//  Copyright © 2019 blueicon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyLib.
+FOUNDATION_EXPORT double SwiftyLibVersionNumber;
+
+//! Project version string for SwiftyLib.
+FOUNDATION_EXPORT const unsigned char SwiftyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLib/PublicHeader.h>
+
+
