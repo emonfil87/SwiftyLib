b91d47df8e694f91c2369653bb46957efe108b41
emonfil <emonfil@yosoyblueicon.com>
2019-09-02 13:03:33 -0500

    added test cases

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Erick Monfil on 8/31/19.
+//  Copyright © 2019 blueicon. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+    
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b : Int) -> Int {
+        return a - b
+    }
+    
+}
